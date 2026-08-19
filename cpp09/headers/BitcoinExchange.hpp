5b6ca15d748b97a5e43e559f609ea902d8d1304b
shugarry <114780801+Shugarry@users.noreply.github.com>
2026-08-19 21:26:51 +0200

    started cpp 09

---
+++ cpp09/headers/BitcoinExchange.hpp
@@ -0,0 +1,16 @@
+#ifndef BITCOIN_EXCHANGE
+# define BITCOIN_EXCHANGE
+
+class BitcoinExchange
+{
+	public:
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange& other);
+		BitcoinExchange& operator=(const BitcoinExchange& other);
+		~BitcoinExchange();
+
+	private:
+
+};
+
+#endif
